d111c777c06930d2e954ffe05f9af81525b727a6
GS <GS@DESKTOP-5PH2BTB>
2025-10-12 13:09:35 +0700

    не работает мышь

--- arkana/arkana.cpp
+++ arkana/arkana.cpp
@@ -45,6 +45,10 @@ rect ball{ gameRect.posX + 400,gameRect.posY + 450,20,20 };
 
 int startBrickPosX = gameRect.posX + 30;
 int startBrickPosY = gameRect.posY + 100;
+step currentBallStep = ballStep;
+float currentX = ball.posX;
+float currentY = ball.posY;
+
 
 POINT p;
 
@@ -57,6 +61,171 @@ ULONG_PTR gdiplusToken;
 HDC hdcBuffer;
 HBITMAP hBitmap;
 RECT clientRect;
+
+//for (int i = 0; i < bricks.size(); i++)
+//{
+//    if (bricks[i].health == 1) color = { 0,255,0 };
+//    if (bricks[i].health == 2) color = { 0,0,255 };
+//    
+//    graphics.FillRectangle(&brickBrush, bricks[i].posX, bricks[i].posY, bricks[i].width, bricks[i].height);
+//    graphics.DrawRectangle(&pen, bricks[i].posX, bricks[i].posY, bricks[i].width, bricks[i].height);
+//}
+//
+//graphics.FillRectangle(&solidBrush, mainrect.posX, mainrect.posY, mainrect.width, mainrect.height);
+//graphics.DrawRectangle(&pen, mainrect.posX, mainrect.posY, mainrect.width, mainrect.height);
+//
+//graphics.FillEllipse(&solidBrush, ball.posX, ball.posY, ball.width, ball.height);
+//graphics.DrawEllipse(&ballPen, ball.posX, ball.posY, ball.width, ball.height);
+
+void DrawingGameRect(Graphics& graf, Pen& bgpen, SolidBrush& bgBrush, HatchBrush& brush1, HatchBrush& brush2) {
+    graf.DrawRectangle(&bgpen, gameRect.posX - 1, gameRect.posY - 1, gameRect.width + 1, gameRect.height + 1);
+    graf.FillRectangle(&bgBrush, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
+    graf.FillRectangle(&brush1, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
+    graf.FillRectangle(&brush2, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
+}
+
+void DrawingMainRect(Graphics &graf, SolidBrush &solidBrush, Pen &pen) {
+    graf.DrawRectangle(&pen, mainrect.posX - 1, mainrect.posY - 1, mainrect.width + 1, mainrect.height + 1);
+    graf.FillRectangle(&solidBrush, mainrect.posX, mainrect.posY, mainrect.width, mainrect.height);
+}
+
+void DrawingBall(Graphics& graf, Pen& ballPen, SolidBrush& solidBrush) {
+    graf.DrawEllipse(&ballPen, ball.posX - 1, ball.posY - 1, ball.width + 1, ball.height + 1);
+    graf.FillEllipse(&solidBrush, ball.posX, ball.posY, ball.width, ball.height);
+}
+
+void DrawingBricks(Graphics& graf, Pen& pen) {
+    for (auto brick : bricks) {
+        if (brick.health == 1) color = { 0,255,0 };
+        else color = { 0,0,255 };
+        SolidBrush brickBrush(color);
+        graf.FillRectangle(&brickBrush, brick.posX, brick.posY, brick.width, brick.height);
+        graf.DrawRectangle(&pen, brick.posX, brick.posY, brick.width, brick.height);
+    }
+}
+void move(float currentStep,float distance) {
+    
+    if (currentStep + 1 >= distance && !debug) {
+        ballStep = currentBallStep;
+        ball.posX = currentX;
+        ball.posY = currentY;
+
+    }
+
+}
+void debuging() {
+    
+        GetCursorPos(&p);
+        ball.posX = p.x;
+        ball.posY = p.y;
+    
+}
+
+void checkMainRectCollisions(float dotx,float doty,float ballCenterX,bool &reflectedThisStep) {
+    if (dotx > mainrect.posX && dotx < mainrect.posX + mainrect.width &&
+        doty > mainrect.posY && doty < mainrect.posY + mainrect.height) {
+
+        int x1 = 0, x2 = 25, y2 = 25;
+        int y1 = abs((mainrect.posX + mainrect.width / 2) - ballCenterX);
+        float degrees = (((x1 * y1 + x2 * y2) / ((sqrt(x1 * x1 + x2 * x2)) * (sqrt(y1 * y1 + y2 * y2)))) * 180 / 3.14f);
+        float t_angle = (100 * (90 - degrees)) / 90;
+        int stepX = round((fullstep * t_angle) / 100);
+        int stepY = round(fullstep - stepX);
+
+        if (ballCenterX > mainrect.posX + mainrect.width / 2) {
+            currentBallStep.x = stepX;
+            currentBallStep.y = stepY;
+        }
+        else if (ballCenterX == mainrect.posX + mainrect.width / 2) {
+            currentBallStep.x = 0;
+            currentBallStep.y = fullstep;
+        }
+        else {
+            currentBallStep.x = -stepX;
+            currentBallStep.y = stepY;
+        }
+
+        reflectedThisStep = true;
+    }
+}
+void checkGameRectCollisions(float dotx,float doty,bool &reflectedThisStep) {
+    if (dotx < gameRect.posX && !reflectedThisStep) {
+        currentBallStep.x *= -1;
+        currentX = gameRect.posX + 1;
+        reflectedThisStep = true;
+    }
+
+    if (dotx > gameRect.posX + gameRect.width && !reflectedThisStep) {
+        currentBallStep.x *= -1;
+        currentX = gameRect.posX + gameRect.width - ball.width - 1;
+        reflectedThisStep = true;
+    }
+
+    if (doty < gameRect.posY && !reflectedThisStep) {
+        currentBallStep.y *= -1;
+        currentY = gameRect.posY + 1;
+        reflectedThisStep = true;
+    }
+
+    if (doty > gameRect.posY + gameRect.height && !reflectedThisStep) {
+        currentBallStep.y *= -1;
+        currentY = gameRect.posY + gameRect.height - 1 - ball.width;
+        reflectedThisStep = true;
+        /*PostMessage(hWnd, WM_CLOSE, 0, 0);*/
+    }
+}
+void checkBricksCollisions(float dotx,float doty,float distance,std::vector<int> hitBricks,bool &reflectedThisStep) {
+    for (int h = 0; h < bricks.size(); h++) {
+        if (dotx + currentBallStep.x / distance > bricks[h].posX && dotx + currentBallStep.x / distance < bricks[h].posX + bricks[h].width &&
+            doty - currentBallStep.y / distance > bricks[h].posY && doty - currentBallStep.y / distance < bricks[h].posY + bricks[h].height) {
+
+            if (std::find(hitBricks.begin(), hitBricks.end(), h) == hitBricks.end()) {
+                if (!reflectedThisStep) {
+                    hitBricks.push_back(h);
+
+                    int colLeft = dotx - bricks[h].posX;
+                    int colRight = (bricks[h].posX + bricks[h].width) - dotx;
+                    int colTop = doty - bricks[h].posY;
+                    int colBottom = (bricks[h].posY + bricks[h].height) - doty;
+                    int minCol = min(min(colLeft, colRight), min(colTop, colBottom));
+
+                    if (min(colLeft, colRight) == min(colTop, colBottom)) {
+                        if (colTop == min(colLeft, colRight)) {
+                            currentBallStep.y = abs(currentBallStep.y) * -1;
+                            currentBallStep.x *= -1;
+                        }
+                        else {
+                            currentBallStep.y = abs(currentBallStep.y);
+                            currentBallStep.x *= -1;
+                        }
+                    }
+                    else if (minCol == colTop || minCol == colBottom) {
+                        currentBallStep.y *= -1;
+                    }
+                    else if (minCol == colLeft || minCol == colRight) {
+                        currentBallStep.x *= -1;
+                    }
+
+                    currentX -= currentBallStep.x / distance;
+                    currentY += currentBallStep.y / distance;
+
+                    reflectedThisStep = true;
+
+                    if (--bricks[h].health <= 0) {
+                        bricks.erase(bricks.begin() + h);
+                        h--;
+                    }
+                    if (bricks.size() == 0) {
+                        PostMessage(hWnd, WM_CLOSE, 0, 0);
+                    }
+                    score += 500;
+                    break;
+                }
+            }
+
+        }
+    }
+}
 
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -125,59 +294,50 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
                 startBrickPosX = gameRect.posX + 30;
             }
         }
+
     case WM_PAINT:
     {
+        
+
         SetTimer(hWnd, 1, 30, NULL);
 
+       
+       Pen pen(Color(0, 0, 0), 2.0f);
+       Pen ballPen(Color(0, 0, 0));
+       Pen bgPen(Color::Black, 10.f);
+       SolidBrush solidBrush(Color::Red);
+       SolidBrush bgBrush(Color(240, 245, 255));
+       HatchBrush brush1(HatchStyleDiagonalCross, Color(180, 160, 196, 255), Color(0, 0, 0, 0));
+       HatchBrush brush2(HatchStyleDottedGrid, Color(80, 255, 154, 162), Color(0, 0, 0, 0));
+       
+
         PAINTSTRUCT ps;
+
         HDC hdc = BeginPaint(hWnd, &ps);
+
         GetClientRect(hWnd, &clientRect);
         hdcBuffer = CreateCompatibleDC(hdc);
         hBitmap = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
         SelectObject(hdcBuffer, hBitmap);
+
         Graphics graphics(hdcBuffer);
         graphics.Clear(Color(160, 165, 185));
+        
+        DrawingGameRect(graphics,bgPen,bgBrush,brush1,brush2);
+        DrawingMainRect(graphics,solidBrush,pen);
+        DrawingBall(graphics,ballPen,solidBrush);
+        DrawingBricks(graphics, pen);
 
-        Pen pen(Color(0, 0, 0), 2.0f);
-        Pen ballPen(Color(0, 0, 0));
-        Pen bgPen(Color::Black, 10.f);
 
         float comp[6] = { 0.0f, 0.2f, 0.3f, 0.7f, 0.8f, 1.0f };
         bgPen.SetCompoundArray(comp, 6);
-
-        SolidBrush solidBrush(Color::Red);
-        SolidBrush bgBrush(Color(240, 245, 255));
-        HatchBrush brush1(HatchStyleDiagonalCross, Color(180, 160, 196, 255), Color(0, 0, 0, 0));
-        HatchBrush brush2(HatchStyleDottedGrid, Color(80, 255, 154, 162), Color(0, 0, 0, 0)
-        );
-
-        graphics.DrawRectangle(&bgPen, gameRect.posX - 1, gameRect.posY - 1, gameRect.width + 1, gameRect.height + 1);
-        graphics.FillRectangle(&bgBrush, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
-        graphics.FillRectangle(&brush1, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
-        graphics.FillRectangle(&brush2, gameRect.posX, gameRect.posY, gameRect.width, gameRect.height);
-
-        //отрисовка блоков
-        for (int i = 0; i < bricks.size(); i++)
-        {
-            if (bricks[i].health == 1) color = { 0,255,0 };
-            if(bricks[i].health == 2) color = { 0,0,255 };
-            SolidBrush brickBrush(color);
-            graphics.FillRectangle(&brickBrush, bricks[i].posX, bricks[i].posY, bricks[i].width, bricks[i].height);
-            graphics.DrawRectangle(&pen, bricks[i].posX, bricks[i].posY, bricks[i].width, bricks[i].height);
-        }
-
-        graphics.FillRectangle(&solidBrush, mainrect.posX, mainrect.posY, mainrect.width, mainrect.height);
-        graphics.DrawRectangle(&pen, mainrect.posX, mainrect.posY, mainrect.width, mainrect.height);
-        graphics.FillEllipse(&solidBrush, ball.posX, ball.posY, ball.width, ball.height);
-        graphics.DrawEllipse(&ballPen, ball.posX, ball.posY, ball.width, ball.height);
-        //
+        
+        
+        
         
         float d = sqrtf(ballStep.x * ballStep.x + ballStep.y * ballStep.y);
         int R = ball.width / 2;
 
-        step currentBallStep = ballStep;
-        float currentX = ball.posX;
-        float currentY = ball.posY;
 
         for (float j = 0; j < d; j++) {
             float step_dx = currentBallStep.x / d;
@@ -201,126 +361,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
                 float dotx = ballCenterX + R * cos(ri);
                 float doty = ballCenterY + R * sin(ri);
 
-                for (int h = 0; h < bricks.size(); h++) {
-                    if (dotx + currentBallStep.x / d > bricks[h].posX && dotx + currentBallStep.x / d < bricks[h].posX + bricks[h].width &&
-                        doty - currentBallStep.y / d > bricks[h].posY && doty - currentBallStep.y / d < bricks[h].posY + bricks[h].height) {
 
-                        if (std::find(hitBricks.begin(), hitBricks.end(), h) == hitBricks.end()) {
-                            if (!reflectedThisStep) {
-                                hitBricks.push_back(h);
-
-                                int colLeft = dotx - bricks[h].posX;
-                                int colRight = (bricks[h].posX + bricks[h].width) - dotx;
-                                int colTop = doty - bricks[h].posY;
-                                int colBottom = (bricks[h].posY + bricks[h].height) - doty;
-                                int minCol = min(min(colLeft, colRight), min(colTop, colBottom));
-
-                                if (min(colLeft, colRight) == min(colTop, colBottom)) {
-                                    if(colTop == min(colLeft,colRight)){
-                                        currentBallStep.y = abs(currentBallStep.y) * -1;
-                                        currentBallStep.x *= -1;
-                                    }
-                                    else {
-                                        currentBallStep.y = abs(currentBallStep.y);
-                                        currentBallStep.x *= -1;
-                                    }
-                                }
-                                else if (minCol == colTop || minCol == colBottom) {
-                                    currentBallStep.y *= -1;
-                                }
-                                else if (minCol == colLeft || minCol == colRight) {
-                                    currentBallStep.x *= -1;
-                                }
-
-                                currentX -= currentBallStep.x / d;
-                                currentY += currentBallStep.y / d;
-
-                                reflectedThisStep = true;
-
-                                if (--bricks[h].health <= 0) {
-                                    bricks.erase(bricks.begin() + h);
-                                    h--;
-                                }
-                                if (bricks.size() == 0) {
-                                    PostMessage(hWnd, WM_CLOSE, 0, 0);
-                                }
-                                score += 500;
-                                break;
-                            }
-                        }
-                        
-                    }
-                }
-
-
-                if (dotx > mainrect.posX && dotx < mainrect.posX + mainrect.width &&
-                    doty > mainrect.posY && doty < mainrect.posY + mainrect.height) {
-
-                    int x1 = 0, x2 = 25, y2 = 25;
-                    int y1 = abs((mainrect.posX + mainrect.width / 2) - ballCenterX);
-                    float degrees = (((x1 * y1 + x2 * y2) / ((sqrt(x1 * x1 + x2 * x2)) * (sqrt(y1 * y1 + y2 * y2)))) * 180 / 3.14f);
-                    float t_angle = (100 * (90 - degrees)) / 90;
-                    int stepX = round((fullstep * t_angle) / 100);
-                    int stepY = round(fullstep - stepX);
-
-                    if (ballCenterX > mainrect.posX + mainrect.width / 2) {
-                        currentBallStep.x = stepX;
-                        currentBallStep.y = stepY;
-                    }
-                    else if (ballCenterX == mainrect.posX + mainrect.width / 2) {
-                        currentBallStep.x = 0;
-                        currentBallStep.y = fullstep;
-                    }
-                    else {
-                        currentBallStep.x = -stepX;
-                        currentBallStep.y = stepY;
-                    }
-
-                    reflectedThisStep = true;
-                }
-
-                if (dotx < gameRect.posX && !reflectedThisStep) {
-                    currentBallStep.x *= -1;
-                    currentX = gameRect.posX + 1;
-                    reflectedThisStep = true;
-                }
-
-                if (dotx > gameRect.posX + gameRect.width && !reflectedThisStep) {
-                    currentBallStep.x *= -1;
-                    currentX = gameRect.posX + gameRect.width - ball.width - 1;
-                    reflectedThisStep = true;
-                }
-
-                if (doty < gameRect.posY && !reflectedThisStep) {
-                    currentBallStep.y *= -1;
-                    currentY = gameRect.posY + 1;
-                    reflectedThisStep = true;
-                }
-
-                if (doty > gameRect.posY + gameRect.height && !reflectedThisStep) {
-                    currentBallStep.y *= -1;
-                    currentY = gameRect.posY+gameRect.height - 1 - ball.width;
-                    reflectedThisStep = true;
-                    /*PostMessage(hWnd, WM_CLOSE, 0, 0);*/
-                }
-
+                checkMainRectCollisions(dotx,doty,ballCenterX,reflectedThisStep);
+                checkGameRectCollisions(dotx, doty, reflectedThisStep);
+                checkBricksCollisions(dotx, doty,d,hitBricks, reflectedThisStep);
                 
                 SetPixel(hdcBuffer, dotx, doty, Color::Black);
             }
-
-            if (j + 1 >= d && !debug) {
-                ballStep = currentBallStep;
-                ball.posX = currentX;
-                ball.posY = currentY;
-                
-
+            if (!debug) {
+                move(j, d);
             }
-
-            if (debug) {
-                GetCursorPos(&p);
-                ball.posX = p.x;
-                ball.posY = p.y;
-                
+            else {
+                debuging();
             }
         }
 
